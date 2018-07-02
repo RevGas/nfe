2d582263451189f5b3384c99c8abe1c2a6b22d0c
Higo Soares Matos <higomatos@gmail.com>
2018-07-02 09:04:09 -0300

    Adicionar Stubs de Retorno de Autorização da NFe 4.0 para BA e SP

---
+++ src/main/java/br/inf/portalfiscal/nfe/wsdl/nferetautorizacao4/sp/hom/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Serviço destinado a retornar o resultado do processamento do lote de NF-e.
+ * 
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRetAutorizacao4")
+package br.inf.portalfiscal.nfe.wsdl.nferetautorizacao4.sp.hom;
