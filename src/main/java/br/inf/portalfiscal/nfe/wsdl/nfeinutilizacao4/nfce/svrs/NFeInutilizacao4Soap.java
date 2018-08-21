50e51e2bccac4b7cc0539df963bbfc32f29fdd81
DanielDPN <danielpereiradonascimento@gmail.com>
2018-08-21 11:34:05 -0300

    Adicionado handler pra Inutilização de NFC-e SVRS Produção

--- src/main/java/br/inf/portalfiscal/nfe/wsdl/nfeinutilizacao4/nfce/svrs/NFeInutilizacao4Soap.java
+++ src/main/java/br/inf/portalfiscal/nfe/wsdl/nfeinutilizacao4/nfce/svrs/NFeInutilizacao4Soap.java
@@ -17,7 +17,7 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 @WebService(name = "NFeInutilizacao4Soap", targetNamespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeInutilizacao4")
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 @XmlSeeAlso({
-    ObjectFactory.class
+    ObjectFactory.class, br.inf.portalfiscal.nfe.ObjectFactory.class, org.w3._2000._09.xmldsig_.ObjectFactory.class
 })
 public interface NFeInutilizacao4Soap {
 
