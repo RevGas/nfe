35b0971bd44b83230f192a7d8e7f1a3ac73ee7ac
Rafael Ferreira da Costa <rafael@tartigrado.com>
2019-05-25 10:25:30 -0300

    Adicionando handler ao ambiente autorizador de PE

--- src/main/java/br/inf/portalfiscal/nfe/wsdl/nferecepcaoevento4/pe/hom/NFeRecepcaoEvento4.java
+++ src/main/java/br/inf/portalfiscal/nfe/wsdl/nferecepcaoevento4/pe/hom/NFeRecepcaoEvento4.java
@@ -2,6 +2,7 @@ package br.inf.portalfiscal.nfe.wsdl.nferecepcaoevento4.pe.hom;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.jws.HandlerChain;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import javax.xml.ws.WebEndpoint;
@@ -20,6 +21,7 @@ import javax.xml.ws.WebServiceFeature;
  * 
  */
 @WebServiceClient(name = "NFeRecepcaoEvento4", targetNamespace = "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4", wsdlLocation = "https://nfehomolog.sefaz.pe.gov.br/nfe-service/services/NFeRecepcaoEvento4?wsdl")
+@HandlerChain(file="handler.xml")
 public class NFeRecepcaoEvento4
     extends Service
 {
