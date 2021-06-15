dd2b2365084149d897525883e5bebf1c36bd18f2
Artur Oliveira <kelly@tartigrado.com>
2021-06-15 16:02:25 -0300

    removidas dependências externas

--- src/main/java/com/fincatto/documentofiscal/cte300/dacte/CTeDacteReport.java
+++ src/main/java/com/fincatto/documentofiscal/cte300/dacte/CTeDacteReport.java
@@ -2,14 +2,24 @@ package com.fincatto.documentofiscal.cte300.dacte;
 
 import com.fincatto.documentofiscal.cte300.classes.nota.CTeProcessado;
 import com.fincatto.documentofiscal.persister.DFPersister;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
 import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -59,6 +69,7 @@ public class CTeDacteReport {
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("NOTAS_FISCAIS_T1", notas1);
             parameters.put("NOTAS_FISCAIS_T2", notas2);
+            parameters.put("QR_CODE", gerarQRCode());
 
             return JasperFillManager.fillReport(in, parameters,  new JRXmlDataSource(convertStringXMl2DOM(), "/cteProc"));
         }
@@ -71,4 +82,35 @@ public class CTeDacteReport {
             return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource);
         }
     }
+
+    public BufferedImage gerarQRCode() throws WriterException, IOException, JAXBException {
+        int size = 250;
+        Map<EncodeHintType, Object> hintMap = new EnumMap<>(EncodeHintType.class);
+        hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix byteMatrix = qrCodeWriter.encode(this.cte.getCte().getInfoSupl().getQrCode(),
+                BarcodeFormat.QR_CODE, size, size, hintMap);
+        int crunchifyWidth = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(crunchifyWidth, crunchifyWidth, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, crunchifyWidth, crunchifyWidth);
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < crunchifyWidth; i++) {
+            for (int j = 0; j < crunchifyWidth; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+        return image;
+    }
+
 }
